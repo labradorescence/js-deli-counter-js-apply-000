78badae3c4a729ad5dfd40889a87256d8420ef37
Greem <orangenewyorkcreativeagency@gmail.com>
2020-02-24 16:15:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,32 +14,32 @@ const takeANumber = (currentLine) => {
   return `Welcome, your ticket number is ${n}.`
 }
 
-const nowServing = (currentLine) => {
-  console.log('NOW SERVING currentLine length falsey', currentLine)
-  if(currentLine.length === 0){
-    return "There is nobody waiting to be served!"
-  }else{
-      console.log('NOW SERVING currentLine length truthy', currentLine)
-      return `Currently serving ${currentLine.shift()}.`
-      console.log('currentLine.shift', currentLine.shift)
-//       pop(): Remove an item from the end of an array.
-//       push(): Add items to the end of an array.
-//       shift(): Remove an item from the beginning of an array.
-//      unshift(): Add items to the beginning of an array.
-
-    }
-  }
-
-const currentLine = (currentLine) => {
-  if(!currentLine.length){
-  return `The line is currently empty.`
-}else{
-
-  let lineNameNum = [];
-console.log('CURRENT LINE console: currentLine', currentLine)
-  for(let i = 0; i < currentLine.length; i++){
-    lineNameNum.push(`${i+1}. ${currentLine[i]}`)
-  }
-  return `The line is currently: ${lineNameNum.join(', ')}`
-}
-}
+// const nowServing = (currentLine) => {
+//   console.log('NOW SERVING currentLine length falsey', currentLine)
+//   if(currentLine.length === 0){
+//     return "There is nobody waiting to be served!"
+//   }else{
+//       console.log('NOW SERVING currentLine length truthy', currentLine)
+//       return `Currently serving ${currentLine.shift()}.`
+//       console.log('currentLine.shift', currentLine.shift)
+// //       pop(): Remove an item from the end of an array.
+// //       push(): Add items to the end of an array.
+// //       shift(): Remove an item from the beginning of an array.
+// //      unshift(): Add items to the beginning of an array.
+//
+//     }
+//   }
+//
+// const currentLine = (currentLine) => {
+//   if(!currentLine.length){
+//   return `The line is currently empty.`
+// }else{
+//
+//   let lineNameNum = [];
+// console.log('CURRENT LINE console: currentLine', currentLine)
+//   for(let i = 0; i < currentLine.length; i++){
+//     lineNameNum.push(`${i+1}. ${currentLine[i]}`)
+//   }
+//   return `The line is currently: ${lineNameNum.join(', ')}`
+// }
+// }
